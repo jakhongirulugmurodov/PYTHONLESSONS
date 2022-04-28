a8186573851c4cddd7a3255abe5b2fa6028f216e
Jakhongir <104246315+jakhongirulugmurodov@users.noreply.github.com>
2022-04-28 12:22:54 +0300

    Create 5lesson(string).py

---
+++ Lessons of Python/5lesson(string).py
@@ -0,0 +1,15 @@
+#kocha = "bog'bon"
+#mahalla = "Sog'bon"
+#tuman = "Bodomzor"
+#viloyat = "Samarqand"
+#print(kocha.capitalize(), 'ko\'chasi,', mahalla, 'mahallasi,', tuman, 'tumani,', viloyat, 'viloyati.')
+
+print("Iltimos ma'lumotlaringizni kiriting=>>>")
+uy = input("Uyingizni tartib raqamini kiriting:")
+kocha = input("Ko'changizni nomi:")
+mahalla = input("Mahallangizni nomi:")
+tuman = input("Tumaningiz nomi:")
+viloyat = input("Viloyatingizni kiriting:")
+
+print('Siz ushbu manzilda istiqomat qilasiz:', viloyat.capitalize(), 'viloyati,', tuman.capitalize(), 'tumani,', mahalla.capitalize(), 'mahallasi,', kocha.capitalize(), 'ko\'chasi.')
+print("Siz biz bilan telegram orqali istalgan daqiqada bog'lanishingiz mumkin!\nBizning telegram manzilimiz: @ume_news")
